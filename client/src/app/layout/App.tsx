fad1dc6aa7d4ce8741fbe0c9dca647f3bfde4a11
barisuu <barisuunsal@gmail.com>
2025-04-18 02:05:20 +0300

    Added view details and create activity card, up until course 49

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,41 +1,37 @@
 import {useEffect, useState} from 'react'
-import {Container, createTheme, CssBaseline, List, ListItem, ListItemText, ThemeProvider} from "@mui/material";
+import {Box, Container, CssBaseline, } from "@mui/material";
 import axios from "axios";
 import NavBar from './NavBar';
-
-const theme = createTheme({
-    palette: {
-        background: {
-            default: "#242424",
-        },
-        text:{
-            primary: 'rgba(255, 255, 255, 0.87)',
-        },
-    },
-});
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 function App() {
     const [activities, setActivities] = useState<Activity[]>([]);
+    const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
 
     useEffect(() => {
         axios.get<Activity[]>('https://localhost:5001/api/activities').then(response => setActivities(response.data))
     }, [])
+    
+    const handleSelectActivity = (id: string) => {
+        setSelectedActivity(activities.find(activity => activity.id === id));
+    }
+    
+    const handleCancelSelectActivity = () => {
+        setSelectedActivity(undefined);
+    }
+    
     return (
-        <>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <NavBar/>
-                <Container maxWidth="xl" sx={{mt:3}}>
-                    <List>
-                        {activities.map(activity => (
-                            <ListItem key={activity.id}>
-                                <ListItemText>{activity.title}</ListItemText>
-                            </ListItem>
-                        ))}
-                    </List>
-                </Container>
-            </ThemeProvider>
-        </>
+        <Box sx={{bgcolor: 'rgb(19,37,90)'}}>
+            <CssBaseline/>
+            <NavBar/>
+            <Container maxWidth="xl" sx={{mt: 3}}>
+                <ActivityDashboard activities={activities} 
+                                   selectActivity={handleSelectActivity} 
+                                   cancelSelectActivity={handleCancelSelectActivity}
+                                   selectedActivity={selectedActivity}
+                />
+            </Container>
+        </Box>
     )
 }
 
---
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,31 @@
+﻿import {Grid} from "@mui/material";
+import ActivityList from "./ActivityList";
+import ActivityDetail from "../details/ActivityDetail";
+import ActivityForm from "../form/ActivityForm";
+
+type Props = {
+    activities: Activity[]
+    selectActivity: (id: string) => void
+    cancelSelectActivity: () => void
+    selectedActivity: Activity | undefined
+}
+
+export default function ActivityDashboard({activities, selectedActivity, selectActivity, cancelSelectActivity}: Props) {
+    return (
+        <Grid container spacing={3}>
+            <Grid size={7}>
+                <ActivityList
+                    activities={activities}
+                    selectActivity={selectActivity}
+                />
+            </Grid>
+            <Grid size={5}>
+                {selectedActivity && <ActivityDetail 
+                    activity={selectedActivity} 
+                    cancelSelectActivity={cancelSelectActivity} 
+                />}
+                <ActivityForm/>
+            </Grid>
+        </Grid>
+    )
+}
